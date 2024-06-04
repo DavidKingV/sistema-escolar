8c4e89188e12835fc72f1ee9e90c0b926f96a947
DavidKingV <qet135ui@hotmail.com>
2024-06-04 15:11:04 -0600

    se corrigen errores de datatables

--- php/subjects/index.php
+++ php/subjects/index.php
@@ -51,7 +51,7 @@ class SubjectsControl{
                 }else{
                     $subjects[] = array("success" => false, "message" => "No se encontraron materias");
                 }
-                error_log("Array final de materias: " . json_encode($subjects));
+                error_log("Array final de materias: " . $subjects);
                 $this->con->close();
                 return $subjects;
             }
