6d9788fc8709d2c019404241e2d28f170aa6c404
DavidKingV <qet135ui@hotmail.com>
2024-06-18 17:18:36 -0600

    SE AGREGAN FUNCIONES NAMESPACE

---
+++ php/src/auth.php
@@ -0,0 +1,25 @@
+<?php
+namespace Vendor\Schoolarsystem;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class auth {
+
+    public static function verify($jwt){
+        loadEnv::cargar();
+        
+        $secretKey = $_ENV['KEY'] ?? NULL;
+
+        if(isset($_SESSION['userId'])&&isset($_COOKIE['auth'])){
+            try {
+                $decoded = JWT::decode($jwt, new Key($secretKey, 'HS256'));     
+                return array('success' => true, 'userId' => $decoded->userId);
+            } catch (\Exception $e) {
+                return array('success' => false, 'message' => $e->getMessage());
+            }
+        }else{
+            return array('success' => false, 'message' => 'No tiene permisos para realizar esta acción');
+        }
+    }
+
+}
