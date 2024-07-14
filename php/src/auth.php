4afac7d0a20273e2c5369bc5292150cddedc5670
DavidKingV <qet135ui@hotmail.com>
2024-07-14 13:19:34 -0600

    se actualiza la función para verificar la sesion iactiva

--- php/src/auth.php
+++ php/src/auth.php
@@ -7,7 +7,8 @@ class auth {
 
     public static function verify($jwt){
         loadEnv::cargar();
-        
+
+        $microsoftAccessToken = $_SESSION["adnanhussainturki/microsoft"]["accessToken"] ?? NULL;
         $secretKey = $_ENV['KEY'] ?? NULL;
 
         if(isset($_SESSION['userId'])&&isset($_COOKIE['auth'])){
@@ -17,7 +18,10 @@ class auth {
             } catch (\Exception $e) {
                 return array('success' => false, 'message' => $e->getMessage());
             }
-        }else{
+        } else if(isset($microsoftAccessToken)){
+            return array('success' => true, 'accessToken' => $microsoftAccessToken);
+        }
+        else{
             return array('success' => false, 'message' => 'No tiene permisos para realizar esta acción');
         }
     }
