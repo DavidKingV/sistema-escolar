8c3b601470ee8cd6fee24015bb8c5bb7824baad9
DavidKingV <qet135ui@hotmail.com>
2024-07-28 13:17:16 -0600

    minor changes

--- php/login/MicrosoftLogin.php
+++ php/login/MicrosoftLogin.php
@@ -10,7 +10,7 @@ loadEnv::cargar();
 $tenant = $_ENV['TENANT_ID'];
 $client_id = $_ENV['CLIENT_ID'];
 $client_secret = $_ENV['CLIENT_SECRET'];
-$callback = "http://localhost:8080/CONTROL%20ESCOLAR%20NUEVO/php/login/MicrosoftAuth.php";
+$callback = $_ENV['CALLBACK_PATH']; 
 $scopes = ["User.ReadBasic.All", "offline_access"];
 
 $microsoft = new Auth($tenant, $client_id,  $client_secret, $callback, $scopes);
