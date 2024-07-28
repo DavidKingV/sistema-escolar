3ccd7bcded6adff48308e091e74e5355dbe82044
DavidKingV <qet135ui@hotmail.com>
2024-07-28 12:53:31 -0600

    correciones menores

--- grupos/altas.php
+++ grupos/altas.php
@@ -172,11 +172,11 @@ if (!$VerifySession['success']) {
             <hr>
             <div class="dropdown">
             <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                <img src="<?php echo $userPhoto ?>" alt="" width="32" height="32" class="rounded-circle me-2">
                 <strong><?php echo $userName ?></strong>
             </a>
             <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+                <li><a class="dropdown-item" id="endSession" href="#">Cerrar Sesión</a></li>
             </ul>
             </div>
         </div>
