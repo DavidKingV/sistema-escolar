a47df53241f85606d2ac010015044b6b53eee34e
David Vega <qet135ui@hotmail.com>
2025-03-11 11:50:04 -0600

    se agregan las funciones del calendario

--- public/practicas/calendario.php
+++ public/practicas/calendario.php
@@ -80,6 +80,20 @@ if (!$VerifySession['success']) {
     </div>
 </div>
 
+<div class="modal fade" id="eventDetails" aria-labelledby="eventDetails" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="eventDetailsLabel"></h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" id="eventDetailsBody">
+            
+        </div>
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>
