7284a17ea3c81da1aba3cfb039aa024e1ac51979
Zaywop06 <241271002@alumnos.utn.edu.mx>
2026-08-18 17:14:49 -0600

    Aplica la estructura base al modulo de grupos
    Aplica la estructura base al modulo de grupos

--- public/grupos.php
+++ public/grupos.php
@@ -1,6 +1,7 @@
-<?php include_once __DIR__.'/../backend/views/mainMenu.php'; ?>
+<?php include_once __DIR__ . '/../backend/views/mainMenu.php'; ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,32 +9,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/allMain.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <!-- <link rel="stylesheet" href="assets/css/groups.css"> -->
-    <link href="https://cdn.datatables.net/v/bs5/dt-2.0.7/datatables.min.css" rel="stylesheet">    
+    <link href="https://cdn.datatables.net/v/bs5/dt-2.0.7/datatables.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
     <title>Grupos</title>
 </head>
+
 <body>
-    <div id="globalLoader" style="display: none; position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(33,37,41,0.5); z-index: 2000;">
-        <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-            <div class="spinner-border text-light" role="status" style="width: 3rem; height: 3rem;">
-                <span class="visually-hidden">Cargando...</span>
-            </div>
-        </div>
-    </div>
-      
     <div id="content">
         <div class="container-fluid">
             <div class="d-flex justify-content-between align-items-center mb-4">
-                <h2 class="mb-0">Lista de grupos</h2>
+                <h2 class="mb-0">Lista de Grupos</h2>
                 <a href="grupos/altas.php" class="btn btn-primary">
-                    <i class="fas fa-plus"></i> Agregar Grupo
+                    <i class="bi bi-plus"></i> Agregar Grupo
                 </a>
             </div>
             <div class="card mb-4">
                 <div class="card-header">
-                    <i class="bi bi-list-task"></i> Lista de grupos
+                    <i class="bi bi-person-badge-fill"></i> Grupos Registrados
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
@@ -44,7 +40,7 @@
                                     <th class="text-center">Carrera</th>
                                     <th class="text-center">Clave</th>
                                     <th class="text-center">Nombre</th>
-                                    <th class="text-center">Miembros</th>   
+                                    <th class="text-center">Miembro(s)</th>
                                     <th class="text-center">Acciones</th>
                                 </tr>
                             </thead>
@@ -59,32 +55,44 @@
     </div>
 
 </body>
+
 </html>
 
 <!-- Modal EDIT -->
-<div class="modal fade modal-lg" id="GroupsEditModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="GroupsEditModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h1 class="modal-title fs-5" id="GroupsEditModalLabel">Editar Grupo</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body" id="modalBodyEditGroup">
-        
+<div class="modal fade modal-lg" id="GroupsEditModal" data-bs-backdrop="static" tabindex="-1"
+    aria-labelledby="GroupsEditModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="loader" id="groupEditLoader">
+                <div class="spinner-border text-primary" role="status"></div>
+                <span class="text-muted">Cargando...</span>
+            </div>
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="GroupsEditModalLabel">Editar Grupo</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="modalBodyEditGroup">
+
+            </div>
         </div>
     </div>
-  </div>
 </div>
 
+<script>const BASE_URL = "<?php echo $_ENV['BASE_URL']; ?>";</script>
+
 <!-- Boostrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
 
 <!-- SweetAlert -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- jquery -->
-<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js" integrity="sha256-J8ay84czFazJ9wcTuSDLpPmwpMXOm573OUtZHPQqpEU=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"
+    integrity="sha256-J8ay84czFazJ9wcTuSDLpPmwpMXOm573OUtZHPQqpEU=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 
 <!-- datables -->
